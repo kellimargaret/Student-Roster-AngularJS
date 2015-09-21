a255e9f4f2809f6bcdf078e4d4a9d4d8d9e8837a
Kyle Pratuch & Samantha Maturen <student@epicodus.com>
2015-09-21 09:56:19 -0700

    Add ability to edit and delete names

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -5,5 +5,14 @@ studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
     { name: "Sandy Smith" },
     { name: "Ryan Samuels" },
     { name: "Brentwood Davis" }
-  ]
+  ];
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index, 1);
+  };
 });
